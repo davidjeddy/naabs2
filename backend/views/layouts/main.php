5c4ca0f9662b337e25896a759e806c3a99621553
David J Eddy <me@davidjeddy.com>
2015-03-02 03:05:55 -0500

    Added link to FE to BE main menu.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -37,7 +37,8 @@ AppAsset::register($this);
             if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
             } else {
-                $menuItems[] = ['label' => 'User Accounts', 'url' => ['/userdetails/index']];;
+                $menuItems[] = ['label' => 'User Accounts', 'url' => ['/userdetails/index']];
+                $menuItems[] = ['label' => 'Frontend', 'url' => ['../../../frontend/web/index.php/site/index']];
                 $menuItems[] = [
                     'label'       => 'Logout (' . Yii::$app->user->identity->username . ')',
                     'url'         => ['/site/logout'],
