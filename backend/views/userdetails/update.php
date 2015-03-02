4b1f642b6d573211dd4aed7ebc6fe53e1a151030
David J Eddy <me@davidjeddy.com>
2015-03-02 11:15:58 -0500

    BE/VW/userDetails form changed to AR rather than html form. FE/MD/SignupForm return changed from null to false.

--- backend/views/userdetails/update.php
+++ backend/views/userdetails/update.php
@@ -1,21 +1,45 @@
 <?php
 
 use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model common\models\UserDetails */
+/* @var $userDetails common\models\UserDetails */
 
-$this->title = 'Update User Details: ' . ' ' . $model->id;
+$this->title = 'Update User Details: ' . ' ' . $userDetails->id;
 $this->params['breadcrumbs'][] = ['label' => 'User Details', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $userDetails->id, 'url' => ['view', 'id' => $userDetails->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="user-details-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?php $form = ActiveForm::begin(['id' => 'form-userAccount']);?>
+            <?= $form->field($userAccount, 'username') ?>                
+            <?php //echo $form->field($userAccount, 'password')->passwordInput() ?>
+            <?php //echo $form->field($userAccount, 'password_repeat')->passwordInput() ?>
+        <div class="form-group">
+            <?= Html::submitButton('Update Account', ['class' => 'btn btn-primary', 'name' => 'account-button']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+	<hr />
+
+    <?php $form = ActiveForm::begin(['id' => 'form-userDetails']); ?>
+        <?= $form->field($userDetails, 'f_name') ?>
+        <?= $form->field($userDetails, 'l_name') ?>
+        <?= $form->field($userDetails, 'p_phone') ?>
+        <?= $form->field($userDetails, 'p_email') ?>
+        <?= $form->field($userDetails, 's_question') ?>
+        <?= $form->field($userDetails, 's_answer') ?>
+        <?= $form->field($userDetails, 'role') ?>
+        <?= $form->field($userDetails, 'created_at') ?>
+        <?= $form->field($userDetails, 'updated_at') ?>
+        <?= $form->field($userDetails, 'deleted_at') ?>
+        <div class="form-group">
+            <?= Html::submitButton('Update Details', ['class' => 'btn btn-primary', 'name' => 'account-button']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
 
 </div>
