b4b121372b66e1258bdb19e3daebb0c02550dd1a
David E <me@davidjeddy.com>
2015-11-19 10:25:57 -0500

    REMOVED ability to create users accounts via the BE admin area

--- backend/views/user-details/index.php
+++ backend/views/user-details/index.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create User Details', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php // echo Html::a('Create User Details', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
