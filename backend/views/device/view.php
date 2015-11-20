40393295ef50eb0660ecc865e994439b1dae0983
David E <me@davidjeddy.com>
2015-11-20 00:43:08 -0500

    FIX error on BE VW Device index

--- backend/views/device/view.php
+++ backend/views/device/view.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'model' => $model,
         'attributes' => [
             //'id',
-            'user_id'
+            'user_id',
             'device_name',
             'pass_phrase',
             'expiration',
