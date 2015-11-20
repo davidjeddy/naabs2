3b9ab1c06106c7ea2c71ec104e46bce68e2b0ba3
David E <me@davidjeddy.com>
2015-11-20 01:48:53 -0500

    ADDED paypal config to CMMN config params

--- common/config/params.php
+++ common/config/params.php
@@ -4,17 +4,22 @@ return [
 	'supportEmail'                  => 'support@windnetworks.net',
     'supportPhone'                  => '+1 (352) 577 5127',
 	'user.passwordResetTokenExpire' => 3600,
-    'paypal' => [
-        'currency'      => 'USD',
-        'intent'        => 'sale',
-        'setCancelUrl'  => isset($_SERVER["HTTP_HOST"]) ?: false,
-        'setReturnUrl'  => isset($_SERVER["HTTP_HOST"]) ?: false,
-        'shipping_rate' => 0.00,
-        'tax_rate'      => 0.07,
-        'transaction'   => ['desc' => 'Wireless network access.',],
-        'paypal_client' => [
-            'id'     => '',
-            'secret' => '',
-        ]
-    ]
+    'components' => [
+        'paypal'=> [
+            'class'        => 'marciocamello\Paypal',
+            'clientId'     => 'you_client_id',
+            'clientSecret' => 'you_client_secret',
+            'isProduction' => false,
+            // This is config file for the PayPal system
+            'config'       => [
+                'http.ConnectionTimeOut' => 30,
+                'http.Retry'             => 1,
+                'log.FileName'           => '@runtime/logs/paypal.log',
+                'log.LogEnabled'         => YII_DEBUG ? 1 : 0,
+                'log.LogLevel'           => \marciocamello\Paypal::LOG_LEVEL_FINE,
+                // development (sandbox) or production (live) mode
+                'mode'                   => \marciocamello\Paypal::MODE_SANDBOX, 
+            ]
+        ],
+    ],
 ];
