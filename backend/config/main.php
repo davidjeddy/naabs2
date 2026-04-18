af6231cfae673ca03769e5033a750f3f791d61f0
David Eddy <me@davidjeddy.com>
2026-04-18 12:31:52 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/naabs2

---
+++ backend/config/main.php
@@ -0,0 +1,45 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [
+        'FreeRadius' => [
+            'class' => 'backend\modules\FreeRadius\FreeRadius',
+        ],
+    ],
+    'components' => [
+        'errorHandler'  => [
+            'errorAction' => 'site/error',
+        ],
+        'log'           => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'user'          => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        /*'view' => [
+             'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-advanced-app'
+                ],
+             ],
+        ],*/
+    ],
+    'params' => $params,
+];
---
+++ frontend/views/site/index.php
@@ -0,0 +1,30 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'Naabs 2';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Naabs 2</h1>
+        <p class="lead"></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Some</h2>
+                <p></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+                <p></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Words</h2>
+                <p></p>
+            </div>
+        </div>
+
+    </div>
+</div>
