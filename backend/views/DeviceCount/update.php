0273a727874b6daa73022a75586def480ecfbf27
David Eddy <me@davidjeddy.com>
2026-04-18 12:31:21 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/naabs2

---
+++ backend/views/DeviceCount/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\DeviceCountOptions */
+
+$this->title = 'Update Device Count Options: ' . ' ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Device Count Options', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="device-count-options-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
---
+++ backend/views/timeamount/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\TimeAmountOptions */
+
+$this->title = 'Update Time Amount Options: ' . ' ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Time Amount Options', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="time-amount-options-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
